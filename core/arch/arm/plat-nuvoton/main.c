22203b3253765a66a3e0c416b5b36b3e8b3855fe
Jacky Huang <ychuang3@nuvoton.com>
2024-08-07 11:13:13 +0800

    Load TSI patch image only if TSI H/W is present
    New MA35D1 parts without TSI, the MA35D16F984 and MA35D16F884, have been
    introduced. The assumption that all MA35D1 parts always have TSI is no
    longer valid. This patch adds a check to load the TSI image only if TSI
    hardware is present.
    
    Change-Id: I919ecfaf6311724ae2ce0d7f18854821d9f59f35

--- core/arch/arm/plat-nuvoton/main.c
+++ core/arch/arm/plat-nuvoton/main.c
@@ -188,18 +188,15 @@ int ma35d1_tsi_init(void)
 				}
 			}
 		}
-	}
-
 #ifdef LOAD_TSI_PATCH
-	ret = TSI_Load_Image((uint32_t)virt_to_phys(tsi_patch_image), sizeof(tsi_patch_image));
-	if (ret == 0)
-		EMSG("Load TSI image successful.\n");
-	else
-		EMSG("Load TSI image failed!! %d\n", ret);
+		ret = TSI_Load_Image((uint32_t)virt_to_phys(tsi_patch_image), sizeof(tsi_patch_image));
+		if (ret == 0)
+			EMSG("Load TSI image successful.\n");
+		else
+			EMSG("Load TSI image failed!! %d\n", ret);
 #endif
-
-	// ma35d1_otp_management();
-
+		// ma35d1_otp_management();
+	}
 	return 0;
 }
 
